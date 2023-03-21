7b6ef0a53c88e3697023a88b70370306446e5d8d
kamrul25 <kamrul.coder01@gmail.com>
2023-03-21 09:30:25 +0600

    load countries

---
+++ js/countries.js
@@ -0,0 +1,19 @@
+const loadCountries = () =>{
+    fetch(`https://restcountries.com/v3.1/all`).then(res => res.json()).then(data => displayCountries(data)).catch(err => console.log(err))
+}
+
+const displayCountries = countries =>{
+    console.log(countries);
+    const countryHTML = countries.map(country => getCountryHTML(country));
+    document.getElementById("countries").innerHTML = countryHTML.join(' ');
+}
+ 
+const getCountryHTML = country =>{
+    return `
+        <div class="country">
+            <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}">
+        </div>
+    `
+}
+loadCountries();
